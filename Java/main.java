2b1959b1e040cf2656b5234dbcf592d03e859f12
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2023-10-24 01:44:24 +0000

    Pending changes exported from your codespace

---
+++ Java/main.java
@@ -0,0 +1,49 @@
+import classes.function; // i called the function now if i add the function
+import java.math; // this is java math. it helps so you can do those hard things.
+
+import javax.net.ssl.X509ExtendedTrustManager;
+
+/* in simple terms i don't know much java but i can explain what i did */
+public class main {
+    public static void mainProject(String []args) {
+        function();
+
+        // but wait there is more!
+        project();
+
+        // math is also easy for computers, computers love math and we do too but its super easy for them.
+        math();
+    }
+// i called the main function to start our project, you can call it whatever honestly
+/* but you have to name the file the same as your class or 
+java gonna be like "Aye that aint the correct thing, fix it fool!" 
+
+but cool thing about java is that it is something you tell it to do and it will do it.
+
+if i make it do  2 / 4 x 3^4 which i can not do yet but learning but i can do 
+
+something else
+
+by the way, remember to add the import and package because its gonna be needed to work on your project.
+without, its just gonna be like "nah we aint executing that bro" so remember
+*/
+
+    public static void project(String []args) {
+        System.out.print("this is also something but there is more!"); // the thing is i probably don't know jack crap about java but i am coding it right here.
+        
+        
+        // fact is you can code in one file but what if that causes problems? that's why you need classes. they can help you.
+    }
+
+    public static void math(String []args) {
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+
+        int answer = num1 * num2 / num3;
+
+        System.out.print(answer);
+    }
+
+}
+
