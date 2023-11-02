b4fd80d72aa294643a660069d49f74a9a14d91b1
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2023-11-02 21:52:09 +0000

    Pending changes exported from your codespace

--- Java/classes/functions.java
+++
@@ -1,7 +0,0 @@
-public class function {
-    public static void thisfunction() {
-        bool name = james;
-
-        System.out.print("Hello my name is " + name); // now created a bool(aka a Boolean) and called it in the print
-    }
-}
---
+++ files/java/src/dev/modxdev/tutorial/classes/function.java
@@ -0,0 +1,10 @@
+package java.src.dev.modxdev.tutorial.classes;
+
+
+public final class function {
+    public static void thisfunction(String []args) {
+        String name = "james"; // the name
+
+        System.out.print("Hello my name is " + name); // now created a bool(aka a Boolean) and called it in the print
+    }
+}
