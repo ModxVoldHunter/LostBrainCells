81df005fd8e57193a1b7dfa928efb38497e7eb8c
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2023-11-02 22:56:44 +0000

    Pending changes exported from your codespace

--- Java/src/dev/modxdev/tutorial/classes/function.java
+++ Java/src/dev/modxdev/tutorial/classes/function.java
@@ -1,6 +1,9 @@
+package Java.src.dev.modxdev.tutorial.classes;
+
+
 public class function {
     public static void thisfunction() {
-        bool name = james;
+        String name = "james"; // the name
 
         System.out.print("Hello my name is " + name); // now created a bool(aka a Boolean) and called it in the print
     }
