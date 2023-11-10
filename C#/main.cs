01fd1da7b98cd11f3cf0054e863cbab236049e0c
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2023-11-10 02:55:17 +0000

    Pending changes exported from your codespace

---
+++ C#/main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C#
+{
+    public class main
+    {
+        Console.Writeline("Hello"); // use Visual Studio 2023 with console application template. 
+    }
+}
